4f27c65199384281416d81d8982270d69bb3a574
dong jian <app@jadendong.com>
2020-04-12 17:34:10 +0800

    my centos

--- custom-script.sh
+++ custom-script.sh
@@ -7,3 +7,30 @@ set -eux
 #
 # For example:
 # yum install -y curl wget git tmux firefox xvfb
+
+### 安装 Docker
+# step 1: 安装必要的一些系统工具
+yum install -y yum-utils device-mapper-persistent-data lvm2
+# Step 2: 添加软件源信息
+yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+# Step 3: 更新并安装Docker-CE
+yum makecache fast
+yum -y install docker-ce
+# Step 4: 开启Docker服务
+systemctl start docker 
+# Step 5: 添加开机自启
+systemctl enable docker
+
+### 安装 Kubernetes
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+setenforce 0
+yum install -y kubelet kubeadm kubectl  # 可能会有索引gpg检查失败的情况, 这时请用 yum install -y --nogpgcheck kubelet kubeadm kubectl 安装
+systemctl enable kubelet && systemctl start kubelet
